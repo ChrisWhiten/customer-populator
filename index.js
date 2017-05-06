df7cebdc3b5ea9b4015798f51e0181fc9ee369a1
Chris Whiten <chris.whiten@gmail.com>
2017-05-05 22:52:50 -0400

    Updates to bring creation date, monthly spend, pagination to intercom companies

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function login(baseUrl, username, password, cb) {
 
 function listCustomers(baseUrl, jwt, cb) {
   var options = {
-    url: baseUrl + '/api/customers?filter[fields][name]=true&filter[fields][id]=true&filter[limit]=99999999',
+    url: baseUrl + '/api/customers?filter[fields][name]=true&filter[fields][id]=true&filter[limit]=99999999&filter[include]=devices',
     headers: {
       Authorization: 'Bearer ' + jwt
     }
@@ -49,6 +49,85 @@ function listCustomers(baseUrl, jwt, cb) {
   });
 }
 
+let intercomCompanies = {};
+
+function indexIntercomCompanies(companies) {
+  companies.forEach(c => {
+    intercomCompanies[c.company_id] = c;
+  });
+}
+
+function listIntercomCompanies(client, cb) {
+  client.companies.list()
+  .then(_companies => {
+    console.log(_companies.body.companies);
+
+    indexIntercomCompanies(_companies.body.companies);
+
+    if (_companies.body.pages.page >= _companies.body.pages.total_pages) {
+      return cb();
+    }
+    _processIntercomCompanyPages(client, _companies.body.pages, cb);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+}
+
+function _processIntercomCompanyPages(client, pages, cb) {
+  client.nextPage(pages)
+  .then(_companies => {
+    console.log(_companies.body.companies);
+
+    indexIntercomCompanies(_companies.body.companies);
+
+    if (_companies.body.pages.page >= _companies.body.pages.total_pages) {
+      return cb();
+    }
+    _processIntercomCompanyPages(client, _companies.body.pages, cb);
+  });
+}
+
+let companies = [];
+let companyIndex = 0;
+function updateCompanies(prefix, client) {
+  if (companyIndex >= companies.length) {
+    console.log('all done!');
+    return;
+  }
+
+  const company = companies[companyIndex];
+  let createdAt = new Date().getTime()/1000;
+
+  if (intercomCompanies.hasOwnProperty(company.id)) {
+    createdAt = intercomCompanies[company.id].created_at;
+  }
+
+  client.companies.create({
+    company_id: company.id,
+    name: prefix + company.name,
+    remote_created_at: createdAt,
+    monthly_spend: company.devices.length * 150,
+  })
+  .then(response => {
+    if (!response.ok) {
+      console.log('Error creating company');
+      console.log(response);
+      companyIndex += 1;
+      updateCompanies(prefix, client);
+    } else {
+      console.log('new company:', response.body);
+      companyIndex += 1;
+      updateCompanies(prefix, client);
+    }
+  })
+  .catch(function (err) {
+    console.log('error:', err);
+    companyIndex += 1;
+    updateCompanies(prefix, client);
+  });
+}
+
 function syncIntercomUsers(baseUrl, prefix, username, password, appId, apiKey) {
   login(baseUrl, username, password, function (err, jwt) {
     if (err) {
@@ -57,40 +136,15 @@ function syncIntercomUsers(baseUrl, prefix, username, password, appId, apiKey) {
     }
 
     listCustomers(baseUrl, jwt, function(err, customers) {
+      companies = customers;
       var client = new Intercom.Client({
         appId: appId,
         appApiKey: apiKey
       }).usePromises();
 
-      // creating a company that already exists seems to be a no-op,
-      // so just create a company for each solink customer
-      customers.forEach(function (company) {
-        client.companies.create({
-        company_id: company.id,
-        name: prefix + company.name
-      })
-        .then(function (response) {
-          if (!response.ok) {
-            console.log('Error creating company');
-            console.log(response);
-          } else {
-            console.log('new company:', response.body);
-          }
-        })
-        .catch(function (err) {
-          console.log('error:', err);
-        });
+      listIntercomCompanies(client, () => {
+        updateCompanies(prefix, client);
       });
-
-      // list companies
-      /*client.companies.list()
-        .then(function (companies) {
-          console.log(companies.body);
-        })
-        .catch(function (err) {
-          console.log(err);
-        });*/
-
     });
   });
 }
